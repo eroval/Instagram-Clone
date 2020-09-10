af6aba11ebd2b3cb888d827fc1c0fb1c72ce9494
Denis Zangarov <denisimo_98@yahoo.com>
2020-09-10 09:57:28 +0300

    The Whole Project

---
+++ TestProject/resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,12 @@
+@component('mail::message')
+# Welcome to my Instagram Clone!
+
+This is just a test project.
+
+@component('mail::button', ['url' => '/'])
+Take me Home
+@endcomponent
+
+All the best,<br>
+Denis
+@endcomponent
